839a97a0e9654086a5c74c085b5a1e4f8167dfb4
Shahbaz Saleem <shahbaz.saleem@jeeny.me>
2020-06-22 17:44:07 +0500

    Updates to lib for carthage

---
+++ UIProgressRing/UIProgressRing.h
@@ -0,0 +1,19 @@
+//
+//  UIProgressRing.h
+//  UIProgressRing
+//
+//  Created by jeeny on 22/06/2020.
+//  Copyright © 2020 Saleems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIProgressRing.
+FOUNDATION_EXPORT double UIProgressRingVersionNumber;
+
+//! Project version string for UIProgressRing.
+FOUNDATION_EXPORT const unsigned char UIProgressRingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIProgressRing/PublicHeader.h>
+
+
